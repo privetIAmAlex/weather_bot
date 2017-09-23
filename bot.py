178b6822ccb5a4ce26548e89ee98732ed3fb44bc
Alexander <31483533+privetIAmAlex@users.noreply.github.com>
2017-09-23 19:48:04 +0800

    short message

---
+++ bot.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-  
+import constants
+import telebot
+import pyowm
+import urllib.request as urllib2
+
+bot = telebot.TeleBot(constants.token_tlgrm)
+owm = pyowm.OWM(constants.token_owm, language="ru")
+stick_id = None
+
+def renaming(name):
+    list1 = list("aуеыаоэяиюь")
+    list2 = list(name)
+    if list2[-1] in list1:
+        list2[-1] = "e"
+    else:
+        list2 += "e"
+    return "".join(list2)
+
+city = "Зима"
+observation = owm.weather_at_place(city)
+my_weather = observation.get_weather()
+
+letter = "Погода в <b>{0}</b> сейчас:\n\nТемпература: {1} °C\nВетер: {2} м/с\n{3}\n{4}".format(renaming(city), 
+                                                                                my_weather.get_temperature("celsius")["temp"], 
+                                                                                my_weather.get_wind()["speed"], 
+                                                                                my_weather.get_detailed_status().title(),
+                                                                                my_weather.get_reference_time(timeformat='iso'))
+
+@bot.message_handler(commands=["start"])
+def handle_message(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    keyboard.row("Погода сейчас", "Погода на завтра")
+    keyboard.row("Выбрать город")
+    bot.send_message(message.from_user.id, "Привет!\U0001f604", reply_markup=keyboard)
+
+@bot.message_handler(content_types=["text"])
+def handle_message(message):
+    if message.text == "Погода сейчас":
+        bot.send_message(message.from_user.id, letter, parse_mode="HTML")
+        if(my_weather.get_detailed_status() == "легкий дождь"):
+            stick_id = "CAADAgADEwADn-jJF-fsRxrFhhlZAg"
+        elif (my_weather.get_detailed_status() == "облачно"):
+            stick_id = "CAADAgADEQADn-jJF1FfbuP1dormAg"
+        elif (my_weather.get_detailed_status() == "ясно"):
+            stick_id = "CAADAgADDwADn-jJF3eEyp2XvPhlAg"
+        elif (my_weather.get_detailed_status() == "туманно") or (my_weather.get_detailed_status() == "слегка облачно"):
+            stick_id = "CAADAgADEAADn-jJF1Es_DxZBlSJAg"
+        elif (my_weather.get_detailed_status() == "пасмурно"):
+            stick_id = "CAADAgADEgADn-jJFzNc_T-lE2l6Ag"
+        else:
+            stick_id = "CAADAgAD9wIAAlwCZQO1cgzUpY4T7wI"
+
+        bot.send_sticker(message.from_user.id, stick_id)
+
+    if message.text == "Выбрать город":
+        bot.send_message(message.from_user.id, "Введите название города:")
+
+
+bot.polling(none_stop=True)
+
+
---
+++ constants.py
@@ -0,0 +1,2 @@
+token_tlgrm = "399505636:AAFB3JnhsS6FaJdJtTAozDRKDVvYAG5s3iw"
+token_owm = "e2cc4904ab9d8d9e2c4c5f8bf44bde99"
